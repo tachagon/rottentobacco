72fd6f62bb252493b60699404feba437e7650b07
saasbook <saasbook@saasbook.(none)>
2015-10-01 11:51:44 +0700

    Create app, and make 4.1 Rails Basics: From Zero to CRUD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottentobacco::Application.config.secret_token = '1a3541c69176389e8cf7a1524d0c39b2497bd7ebcbecb1f985d9ce82ee8985c5c10ccb9437a0ec326b9c23b8c0e9be4265f2b7119135aca60190005ff3e5dffc'
