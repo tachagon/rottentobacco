573acfbe93f87bcbb76a2cb5473f9f49cec01dd1
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-02-15 17:49:50 +0000

    omniauth.rb secret Key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "PYFiAKoLo5LAw7QinhxbFNiTb", "kBlgOkVukoSYprkCRwHNSmafBM32tv8kPqreEEYdZf4kj9Mx0y"
-  provider :facebook, '1689219541323576', '2e6547729e7dfc9b69a0137acc92e7c2'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
